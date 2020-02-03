66d6e14772250a897a4c765c753400faf6be0045
JDtheGeek <john.dennehy@gmail.com>
2020-02-03 18:56:49 +0000

    adds test to fizzbuzz to ensure number is not returned if value is 3

--- index.test.js
+++ index.test.js
@@ -1,17 +1,17 @@
 import { fizzbuzz, fizz } from './index.js'
 
 describe('fizzbuzz', () => {
-  it('fizzbuzz', () => {
-    expect(fizzbuzz(1)).toEqual(1)
+  it('returns the number passed to it, if not a multiple of 3 or 5', () => {
+    expect(fizzbuzz(1)).toBe(1)
+    expect(fizzbuzz(3)).not.toBe(3)
   });
-
 });
 
 describe('fizz', () => {
   it('should return fizz if passed a multiple of 3', () => {
-    expect(fizz(3)).toEqual('fizz')
+    expect(fizz(3)).toBe('fizz')
   });
   it('should return the number if it is not a multiple of 3', () => {
-    expect(fizz(2)).toEqual(2)
+    expect(fizz(2)).toBe(2)
   });
 });
