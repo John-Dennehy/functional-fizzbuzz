947304eedc32e05a3fb46031a5762ceea4e4ce0d
JDtheGeek <john.dennehy@gmail.com>
2020-02-03 18:49:55 +0000

    Adds fizz function and test

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 export const fizzbuzz = number => number
+
+export const fizz = number => {
+  if (number === 3) return 'fizz'
+  return number
+}
--- index.test.js
+++ index.test.js
@@ -1,8 +1,17 @@
-import { fizzbuzz } from './index.js'
+import { fizzbuzz, fizz } from './index.js'
 
 describe('fizzbuzz', () => {
   it('fizzbuzz', () => {
     expect(fizzbuzz(1)).toEqual(1)
   });
 
+});
+
+describe('fizz', () => {
+  it('should return fizz if passed a multiple of 3', () => {
+    expect(fizz(3)).toEqual('fizz')
+  });
+  it('should return the number if it is not a multiple of 3', () => {
+    expect(fizz(2)).toEqual(2)
+  });
 });
