93ff78120ff822b3fc09bd825608f6dda60aee6f
JDtheGeek <john.dennehy@gmail.com>
2020-02-04 10:05:15 +0000

    refactors fizzbuzz() to removes isFizzAndBuzz as superflous

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export const fizzbuzz = num => {
   if (isNaN(num)) return 'Invalid Number'
-  if (isFizzAndBuzz(num)) return 'fizzbuzz'
+  if (isFizz(num) && isBuzz(num)) return 'fizzbuzz'
   if (isFizz(num)) return 'fizz'
   if (isBuzz(num)) return 'buzz'
   return num
@@ -8,5 +8,3 @@ export const fizzbuzz = num => {
 
 export const isFizz = num => num % 3 === 0
 export const isBuzz = num => num % 5 === 0
-export const isFizzAndBuzz = num => isFizz(num) && isBuzz(num)
-
